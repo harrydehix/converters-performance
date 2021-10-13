b6e57d9ffa9a62c0440aaea5f47fab3bf55faa0e
harrydehix <harry.holly02@gmail.com>
2021-10-13 16:03:43 +0200

    Disable html output, update package.json

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import case2 from "./cases/case2";
 import case3 from "./cases/case3";
 import case4 from "./cases/case4";
 
-const htmlOutput = true;
+const htmlOutput = false;
 
 case1(htmlOutput);
 case2(htmlOutput);
